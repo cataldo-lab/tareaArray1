e293f4a1eb9bdfe59e8784db0c75602dfc422a49
cataldo-lab <cataldobenjamin@gmail.com>
2026-06-23 13:56:10 -0400

    resumen conceptos js

--- .idea/AlgebraArreglos.js
+++ .idea/AlgebraArreglos.js
@@ -17,3 +17,61 @@ Se utiliza en ECMASScript 6
 
     ... : Spread Operador : operador de propagacion
  */
+
+console.log("______________________________________");
+console.log("______________________________________");
+
+let interseccion = a.filter(valor => b.includes(valor));
+console.log(interseccion);
+
+console.log("______________________________________");
+console.log("______________________________________");
+
+let diferencia = a.filter(valor => !b.includes(valor));
+console.log(diferencia);
+
+console.log("______________________________________");
+console.log("______________________________________");
+
+let diferencia2 = b.filter(valor => !a.includes(valor));
+console.log(diferencia2);
+
+//Depende el orden de diferencia que entrega el resultado.
+
+console.log("______________________________________");
+console.log("______________________________________");
+
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+console.log(matriz[1][2]);
+
+for (let i = 0; i < matriz.length; i++) {
+    console.log(matriz[i]);
+}
+console.log("Recorrer matriz 2D");
+console.log("______________________________");
+console.log("______________________________");
+
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        console.log(`Elemento en fila ${i}, columna ${j}:${matriz[i][j]}`);
+    }
+}
+
+console.log("______________________________");
+console.log("______________________________");
+
+console.log("Inicio de objeto");
+
+let usuario ={
+    nombre: "Sofia",
+    edad:28,
+    bandaFavorita: "Almas Perdidas"
+}
+
+console.log(usuario["nombre"]);
+console.log(usuario["edad"]);
+
---
+++ .idea/metodosArreglo.js
@@ -0,0 +1,29 @@
+//metodos de Arreglos
+
+/*
+Metodos de modificacion del Arreglo
+    push(): Agregar al final
+    pop(): Eliminar al final
+
+    unshift(): Agregar al inicio
+    shift(): Eliminar al inicio
+-------------------------------------
+ */
+console.log("Agregar final: push")
+
+let frutas=["Manzana","Banana"];
+frutas.push("Naranja");
+console.log(frutas);
+
+console.log("________________________________");
+console.log("");
+console.log("Eliminar final: pop()");
+console.log("");
+
+let numeros=[1,2,3];
+let eliminado=numeros.pop();
+console.log(numeros);
+console.log(eliminado);
+
+console.log("________________________________");
+console.log("");
